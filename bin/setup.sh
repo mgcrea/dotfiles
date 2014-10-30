21f089649e52fcc45d834450586e6e5d68e25a83
Olivier Louvignes <olivier@mg-crea.com>
2014-10-30 12:26:09 +0100

    feat(setup): nicer setup

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
+DATE=$(date +%I:%M%p)
+CHECK='\033[32m✔\033[39m'
+HR='##################################################'
+
+echo -e "\n${HR}"
+echo -e "Setting up dotfiles..."
+echo -e "${HR}\n"
 
 p="$HOME/.dotfiles";
 for f in $(find $p -maxdepth 1 -mindepth 1 -type f -print0 | xargs -0); do
 	f=`basename "$f"`
-	echo -n "Prossessing \"$f\"..."
+	echo -n "Linking \"$f\"..."
 	ln -fs ".dotfiles/$f" "$HOME/$f"
-	echo " OK."
+	echo -e "		${CHECK} Done"
 done
 
 if [[ $OSTYPE =~ "darwin" ]]; then
 	echo -n "Prossessing \".profile\"..."
 	ln -fs ".dotfiles/osx/.profile" "$HOME/.profile"
-	echo " OK."
+	echo -e "		${CHECK} Done"
 fi
 
 if [[ $OSTYPE =~ "linux" ]]; then
@@ -19,8 +26,18 @@ if [[ $OSTYPE =~ "linux" ]]; then
 	curl https://raw.github.com/aurora/rmate/master/rmate > rmate
 	sudo mv rmate /usr/local/bin
 	sudo chmod +x /usr/local/bin/rmate
-	echo " OK."
+	echo -e "		${CHECK} Done"
 fi
+
+echo -n "Installing \"Vundle\"..."
+if [[ ! -d ~/.vim/bundle/Vundle.vim ]]; then
+	git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+echo -e "			${CHECK} Done"
+
+echo -e "\n${HR}"
+echo -e "Dotfiles successfully setup at ${DATE}."
+echo -e "${HR}\n"
 
 # echo "Apply osx config"
 # bash "$p/osx/.osx"
